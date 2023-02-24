fd18b5490a87000cedd884f1e0be561650168d0c
Rafael Nicolas <nicolasrafael178@gmail.com>
2023-02-24 14:34:23 -0300

    Add New Routes

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,20 @@
 import { Router } from 'express';
 
+import { sendImage } from './app/useCases/sendImage';
+import { sendList } from './app/useCases/sendList';
+import { sendLocation } from './app/useCases/sendLocation';
 import { sendText } from './app/useCases/sendText';
 
 export const router = Router();
 
-// send Simple Text
+// Send Simple Text
 router.post('/sendText', sendText);
+
+// Send Simple List
+router.post('/sendText', sendList);
+
+// Send Location
+router.post('/sendText', sendLocation);
+
+// Send Image
+router.post('/sendText', sendImage);
